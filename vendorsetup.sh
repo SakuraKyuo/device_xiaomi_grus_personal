a98c986c87f430b3ebdb76f25cfb875eafc6d1d2
unknown <sakurakyuo@protonmail.com>
2024-07-17 10:12:32 +0800

    grus: Switch to new kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,5 +6,5 @@ rm -rf hardware/xiaomi
 # clone
 git clone https://github.com/SakuraKyuo/device_xiaomi_sdm710-common_personal device/xiaomi/sdm710-common -b UDC
 git clone https://github.com/SakuraKyuo/vendor_xiaomi_grus vendor/xiaomi -b UDC
-git clone --depth=1 https://github.com/SakuraKyuo/android_kernel_xiaomi_sdm710-twilight kernel/xiaomi/sdm710 -b 14
+git clone --depth=1 https://github.com/SakuraKyuo-open-source/kernel_xiaomi_sdm710 kernel/xiaomi/sdm710
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
